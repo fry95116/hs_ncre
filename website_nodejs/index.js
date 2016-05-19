5d3468742de1a193c4955e7775811090fe00ce1c
tom smith <fry95116@hotmail.com>
2016-05-19 12:09:13 +0800

    add loader

--- website_nodejs/index.js
+++ website_nodejs/index.js
@@ -1,9 +1,10 @@
 ﻿var express = require('express'),
 
-		app = express(),
+	app = express(),
 
-		front = require('./front_router'),
-		backstage = require('./backstage_router');
+	front = require('./router_front'),
+    backstage = require('./router_backstage'),
+    leader = require('./router_conf_guide.js');
 
 app.set("view engine", "ejs");
 app.set("view options", {
@@ -13,10 +14,10 @@ app.set("view options", {
 //处理静态文件
 app.use('/', express.static(__dirname + '/static'));
 
-app.use('/',front);
+//app.use('/',front);
+//app.use('/admin',backstage);
 
-app.use('/admin',backstage);
-
+app.use('/',leader);
 
 app.listen(8080, function() {
 	console.log('listening on 8080');
